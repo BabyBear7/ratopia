405e8c75f5485f62dcbde06d320f4ff38067915f
BabyBear7 <soul7687@gmail.com>
2026-04-14 19:28:01 +0900

    Update landing.js

--- app/assets/js/scripts/landing.js
+++ app/assets/js/scripts/landing.js
@@ -244,7 +244,7 @@ const refreshServerStatus = async (fade = false) => {
 
     try {
 
-        const servStat = await getServerStatus(47, serv.hostname)
+        const servStat = await getServerStatus(47, serv.hostname, serv.port)
         console.log(servStat)
         pLabel = Lang.queryJS('landing.serverStatus.players')
         pVal = servStat.players.online + '/' + servStat.players.max
